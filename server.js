9e7923433be94256f405556cfa6c6f514a251347
Waltari10 <vallai10@gmail.com>
2015-07-27 18:42:17 +0300

    asdf

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ function logoutEvent(json, ws, req) {
 
 function rememberSession(ws, email, sessionid) {
 	var jsonReply;
-	console.log('INSERT INTO session (sessionid, email) VALUES ('  + '"' + sessionid + '", ' + mysqlConnection.escape(email) + ')');
+	console.log('INSERT INTO session (sessionid, email) VALUES ('  + '"' + sessionid + '", "' + mysqlConnection.escape(email) + '")');
 	mysqlConnection.query('INSERT INTO session (sessionid, email) VALUES ()'  + '"' + sessionid + '", "' + mysqlConnection.escape(email) + '")', function (err, rows, fields)  {
 		if (err) {
 			jsonReply = {
