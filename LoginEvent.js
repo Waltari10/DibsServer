22bce901fc6767cef62dccf127c0d67b04bbf70c
Waltari10 <vallai10@gmail.com>
2015-07-28 15:50:43 +0300

    syntax fix

--- LoginEvent.js
+++ LoginEvent.js
@@ -1,44 +1,46 @@
 "use strict";
 
-function action(json, ws, req) {
-	var jsonReply;
-	try {
-		console.log("query: " + 'SELECT * FROM user WHERE email = ' + mysqlConnection.escape(json.email));
-		mysqlConnection.query('SELECT * FROM user WHERE email = ' + mysqlConnection.escape(json.email), function (err, rows, fields) {
-			if (err) {
-				throw err;
-			}
-			if (rows.length !== 0) {
-				if (bcrypt.compareSync(json.password, decoder.write(rows[0].password))) {
-					
-					rememberSession("login", ws, json, req.sessionID);
-					jsonReply = {
-						event: "login",
-						email: json.email,
-						sessionid: req.sessionID
-					};
-					ws.send(JSON.stringify(jsonReply));
+module.exports = {
+	action: function (json, ws, req) {
+		var jsonReply;
+		try {
+			console.log("query: " + 'SELECT * FROM user WHERE email = ' + mysqlConnection.escape(json.email));
+			mysqlConnection.query('SELECT * FROM user WHERE email = ' + mysqlConnection.escape(json.email), function (err, rows, fields) {
+				if (err) {
+					throw err;
+				}
+				if (rows.length !== 0) {
+					if (bcrypt.compareSync(json.password, decoder.write(rows[0].password))) {
+						
+						rememberSession("login", ws, json, req.sessionID);
+						jsonReply = {
+							event: "login",
+							email: json.email,
+							sessionid: req.sessionID
+						};
+						ws.send(JSON.stringify(jsonReply));
+					} else {
+						jsonReply = {
+							event: "login",
+							error: "wrong password"
+						};
+						ws.send(JSON.stringify(jsonReply));
+					}
 				} else {
 					jsonReply = {
-						event: "login",
-						error: "wrong password"
-					};
+							event: "login",
+							error: "wrong email"
+						};
 					ws.send(JSON.stringify(jsonReply));
 				}
-			} else {
-				jsonReply = {
-						event: "login",
-						error: "wrong email"
-					};
-				ws.send(JSON.stringify(jsonReply));
-			}
-		});
-	} catch (err) {
-		jsonReply = {
-				event: "login",
-				error: "server error"
-			};
-		ws.send(JSON.stringify(jsonReply));
-		console.log(err);
+			});
+		} catch (err) {
+			jsonReply = {
+					event: "login",
+					error: "server error"
+				};
+			ws.send(JSON.stringify(jsonReply));
+			console.log(err);
+		}
 	}
 }
--- server.js
+++ server.js
@@ -181,49 +181,6 @@ function rememberSession(event, ws, json, sessionid) {
 	});
 }
 
-function loginEvent(json, ws, req) {
-	var jsonReply;
-	try {
-		console.log("query: " + 'SELECT * FROM user WHERE email = ' + mysqlConnection.escape(json.email));
-		mysqlConnection.query('SELECT * FROM user WHERE email = ' + mysqlConnection.escape(json.email), function (err, rows, fields) {
-			if (err) {
-				throw err;
-			}
-			if (rows.length !== 0) {
-				if (bcrypt.compareSync(json.password, decoder.write(rows[0].password))) {
-					
-					rememberSession("login", ws, json, req.sessionID);
-					jsonReply = {
-						event: "login",
-						email: json.email,
-						sessionid: req.sessionID
-					};
-					ws.send(JSON.stringify(jsonReply));
-				} else {
-					jsonReply = {
-						event: "login",
-						error: "wrong password"
-					};
-					ws.send(JSON.stringify(jsonReply));
-				}
-			} else {
-				jsonReply = {
-						event: "login",
-						error: "wrong email"
-					};
-				ws.send(JSON.stringify(jsonReply));
-			}
-		});
-	} catch (err) {
-		jsonReply = {
-				event: "login",
-				error: "server error"
-			};
-		ws.send(JSON.stringify(jsonReply));
-		console.log(err);
-	}
-}
-
 function registerEvent(json, ws, req) {
 	var jsonReply;
 	try {
