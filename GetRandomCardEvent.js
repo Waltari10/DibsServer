8271106cc5f68319814b0d19373df718dd45b8f2
Waltari10 <vallai10@gmail.com>
2015-08-01 16:21:22 +0300

    asdf

--- GetRandomCardEvent.js
+++ GetRandomCardEvent.js
@@ -5,12 +5,13 @@ module.exports = {
 		var jsonReply;
 		try {
 			console.log("SELECT COUNT(*) from card");
-			mysqlConnection.query('SELECT COUNT(*) from card', function (err, result) {
+			mysqlConnection.query('SELECT COUNT(*) from card', function (err, rows, fields) {
 				if (err) {
 					throw err;
 				} 
 				else {
 					console.log("Card count is" + result);
+					console.log(result);
 				}
 			});
 		} catch (err) {
