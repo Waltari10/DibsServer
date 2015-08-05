67d998ec60d4fa4396095c793414515e118e1274
Valtteri <vallai10@gmail.com>
2015-08-05 12:12:35 +0300

    adding profileCards to db

--- SetCardEvent.js
+++ SetCardEvent.js
@@ -4,29 +4,69 @@ module.exports = {
 		Action: function(json, ws, mysqlConnection) {
 		var jsonReply;
 		try {
-			console.log('INSERT INTO card (cardname, picture, stats, email) VALUES (' + mysqlConnection.escape(json.cardname) + ', ' + mysqlConnection.escape(json.picture) + ', ' + mysqlConnection.escape(json.stats) + ', ' + mysqlConnection.escape(json.email) + ')');
-			mysqlConnection.query('INSERT INTO card (cardname, picture, stats, email) VALUES (' + mysqlConnection.escape(json.cardname) + ', ' + mysqlConnection.escape(json.picture) + ', ' + mysqlConnection.escape(json.stats) + ', ' + mysqlConnection.escape(json.email) + ')', function (err, result) {
-				if (err) {
-					jsonReply = {
-						event: "error",
-						error: "server error on setCard SQL"
-					};
-					ws.send(JSON.stringify(jsonReply));
-					throw err;
-				} else {
-					jsonReply = {
-						event: "profileSaved"
-					};
-					ws.send(JSON.stringify(jsonReply));
-				}
-			});
+			if (jsonReply.profileCard == 0) {
+				
+				var query = 'INSERT INTO card (cardname, picture, stats, email, rank, profilecard) VALUES (' + mysqlConnection.escape(json.cardname) + ', ' + mysqlConnection.escape(json.picture) + ', ' + mysqlConnection.escape(json.stats) + ', ' + mysqlConnection.escape(json.email) + ', ' + 1 + ', ' + 0 +')';
+				
+				console.log(query);
+				
+				mysqlConnection.query(query, function (err, result) {
+					if (err) {
+						jsonReply = {
+							event: "error",
+							error: "server error on setCard SQL"
+						};
+						ws.send(JSON.stringify(jsonReply));
+						throw err;
+					} else {
+						jsonReply = {
+							event: "profileSaved"
+						};
+						ws.send(JSON.stringify(jsonReply));
+					}
+				});
+			} else {
+				
+				var query = 'INSERT INTO card (cardname, picture, stats, email, rank, profilecard) VALUES (' 
+				+ mysqlConnection.escape(json.cardname) 
+				+ ', ' + mysqlConnection.escape(json.picture) 
+				+ ', ' + mysqlConnection.escape(json.stats) 
+				+ ', ' + mysqlConnection.escape(json.email) 
+				+ ', 1'
+				+ ', 1' 
+				+ ') ON DUPLICATE KEY UPDATE cardname=VALUES(' + mysqlConnection.escape(json.cardname)
+				+ '), picture=VALUES(' + mysqlConnection.escape(json.picture) 
+				+ '), stats=VALUES(' + mysqlConnection.escape(json.stats) 
+				+ '), email=VALUES(' + mysqlConnection.escape(json.email) 
+				+ '), rank=VALUES(1'
+				+ '), profilecard=VALUES(1' 
+				+ ')';
+				
+				console.log(query);
+				
+				mysqlConnection.query(query, function (err, result) {
+					if (err) {
+						jsonReply = {
+							event: "error",
+							error: "server error on setCard SQL"
+						};
+						ws.send(JSON.stringify(jsonReply));
+						throw err;
+					} else {
+						jsonReply = {
+							event: "profileSaved"
+						};
+						ws.send(JSON.stringify(jsonReply));
+					}
+				});
+			}
 		} catch (err) {
-			jsonReply = {
-					event: "error",
-					error: "server error on setCard"
-				};
-			ws.send(JSON.stringify(jsonReply));
-			console.log(err);
+		jsonReply = {
+				event: "error",
+				error: "server error on setCard"
+			};
+		ws.send(JSON.stringify(jsonReply));
+		console.log(err);
 		}
 	}
 };
