d59aaa0bd81a6aad5dd05dd4a6c0a9f5b1854460
Waltari10 <vallai10@gmail.com>
2015-08-14 10:03:29 +0300

    syn err

--- GetProfileCardEvent.js
+++ GetProfileCardEvent.js
@@ -12,7 +12,7 @@ module.exports = {
 				}
 				if (fields.length !== 0) {
 					
-					var pictureBuffer = new buffer(rows[0].picture);  //Transforming picture from blob binary array to base64 string
+					var pictureBuffer = new Buffer(rows[0].picture);  //Transforming picture from blob binary array to base64 string
 					var pictureBase64 = pictureBuffer.toString('base64');
 					
 					console.log(pictureBase64);
