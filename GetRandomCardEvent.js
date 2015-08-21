5a00a3bf7df1aba5c63f8ea2b7c779bcc4dd3cec
Waltari10 <vallai10@gmail.com>
2015-08-21 10:11:46 +0300

    cleaning code

--- GetRandomCardEvent.js
+++ GetRandomCardEvent.js
@@ -18,43 +18,11 @@ module.exports = {
 					for (var i = 0; i < result.length; i++) {
 						cardIds.push(result[i].idcard);
 					}
-					console.log("cardIds: " + cardIds);
-					/*result.forEach(function(item) {
-							
-						} );*/
-					
 					
 					if (json.seenCards === "") {
-						console.log(cardIds);
 						rand = Math.floor((Math.random() * cardIds.length));
-
-						console.log("rand: " + rand);
-
-						console.log("cardIds[rand]: " + cardIds[rand]);
+						getCardById(cardIds);
 						
-						query = "SELECT 1 FROM card WHERE idcard = " + cardIds[rand];
-						console.log(query);
-						mysqlConnection.query(query , function (err, rows, fields) {
-							if (err) {
-								throw err;
-							} 
-							else {
-								jsonReply = {
-									event: "getRandomCard",
-									cardname: rows[0].cardname,
-									picture: rows[0].picture,
-									rank: rows[0].rank,
-									value: rows[0].value,
-									email: rows[0].email,
-									description: rows[0].description,
-									color: rows[0].color,
-									idcard: rows[0].idcard
-								};
-								
-								console.log(JSON.stringify(jsonReply));
-								ws.send(JSON.stringify(jsonReply));
-							}
-						});
 					} else {
 						
 						
@@ -65,6 +33,36 @@ module.exports = {
 			});
 		} catch (err) {
 			console.log(err);
+		}
+		
+		function getCardById(cardIds) {
+			
+			query = "SELECT 1 FROM card WHERE idcard = " + cardIds[rand];
+			console.log(query);
+			
+			mysqlConnection.query(query , function (err, rows, fields) {
+				if (err) {
+					throw err;
+				} 
+				else {
+					console.log("sql rows: " + rows);
+					
+					jsonReply = {
+						event: "getRandomCard",
+						cardname: rows[0].cardname,
+						picture: rows[0].picture,
+						rank: rows[0].rank,
+						value: rows[0].value,
+						email: rows[0].email,
+						description: rows[0].description,
+						color: rows[0].color,
+						idcard: rows[0].idcard
+					};
+					
+					console.log(JSON.stringify(jsonReply));
+					ws.send(JSON.stringify(jsonReply));
+				}
+			});
 		}
 	}
 };
