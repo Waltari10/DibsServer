8bcce694cae8949b372ca95f35f51b7cae65b762
Waltari10 <vallai10@gmail.com>
2015-08-21 09:43:49 +0300

    async error

--- GetRandomCardEvent.js
+++ GetRandomCardEvent.js
@@ -14,51 +14,55 @@ module.exports = {
 				} 
 				else {
 					cardAmount = result.length;
-					result.forEach(function(item) {
-							cardIds.push(item.idcard);
-						} );
-					console.log(cardIds);
+					
+					for (var i = 0; i < result.length; i++) {
+						cardIds.push(result[i].idcard);
+					}
+					console.log("cardIds: " + cardIds);
+					/*result.forEach(function(item) {
+							
+						} );*/
+					
+					
+					if (json.seenCards === "") {
+						console.log(cardIds);
+						rand = Math.floor((Math.random() * cardIds.length));
+
+						console.log("rand: " + rand);
+
+						console.log("cardIds[rand]: " + cardIds[rand]);
+						
+						query = "SELECT 1 FROM card WHERE idcard = " + cardIds[rand];
+						console.log(query);
+						mysqlConnection.query(query , function (err, rows, fields) {
+							if (err) {
+								throw err;
+							} 
+							else {
+								jsonReply = {
+									event: "getRandomCard",
+									cardname: rows[0].cardname,
+									picture: rows[0].picture,
+									rank: rows[0].rank,
+									value: rows[0].value,
+									email: rows[0].email,
+									description: rows[0].description,
+									color: rows[0].color,
+									idcard: rows[0].idcard
+								};
+								
+								console.log(JSON.stringify(jsonReply));
+								ws.send(JSON.stringify(jsonReply));
+							}
+						});
+					} else {
+						
+						
+					}
+					
 					
 				}
 			});
-			
-			if (json.seenCards === "") {
-				console.log(cardIds);
-				rand = Math.floor((Math.random() * cardIds.length));
-
-				console.log("rand: " + rand);
-
-				console.log("cardIds[rand]: " + cardIds[rand]);
-				
-			} else {
-				
-				
-			}
-			
-			query = "SELECT 1 FROM card WHERE idcard = " + cardIds[rand];
-			console.log(query);
-			mysqlConnection.query(query , function (err, rows, fields) {
-					if (err) {
-						throw err;
-					} 
-					else {
-						jsonReply = {
-							event: "getRandomCard",
-							cardname: rows[0].cardname,
-							picture: rows[0].picture,
-							rank: rows[0].rank,
-							value: rows[0].value,
-							email: rows[0].email,
-							description: rows[0].description,
-							color: rows[0].color,
-							idcard: rows[0].idcard
-						};
-						
-						console.log(JSON.stringify(jsonReply));
-						ws.send(JSON.stringify(jsonReply));
-					}
-				});
-
 		} catch (err) {
 			console.log(err);
 		}
